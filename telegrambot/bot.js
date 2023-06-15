b87db07d4a958c895a0a3f39249d4a900f932968
acat_mad <myli0724@qq.com>
2023-06-15 15:10:18 +0800

    env

--- telegrambot/bot.js
+++ telegrambot/bot.js
@@ -1,15 +1,16 @@
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
+require('dotenv').config();
 
 // 创建 Telegraf 实例
-const botToken = '6102981568:AAHmWGKJKUm_s8PbfiHePCz03hx05LLcM2M';  // 替换为你的 Telegram Bot Token
+const botToken = process.env.TG_BOT;  // 替换为你的 Telegram Bot Token
 const bot = new Telegraf(botToken);
 
 // 处理 /say 命令
 bot.command('say', async (ctx) => {
   try {
     const quoteEndpoint = 'http://localhost:3000/quotes/random'; // 替换为你的 API Endpoint
-    const response = await axios.get(quoteEndpoint);
+    const response = await axios.post(quoteEndpoint);
     const quote = response.data;
     const text = quote.text;
     
